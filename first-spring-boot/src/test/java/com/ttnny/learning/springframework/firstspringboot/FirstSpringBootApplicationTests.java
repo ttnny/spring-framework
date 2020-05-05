7f8953d41eeca26fb6ebd8074f40c6af4901cf14
ttnny <ttnny@users.noreply.github.com>
2020-05-04 23:49:38 -0700

    Update project

---
+++ first-spring-boot/src/test/java/com/ttnny/learning/springframework/firstspringboot/FirstSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttnny.learning.springframework.firstspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
